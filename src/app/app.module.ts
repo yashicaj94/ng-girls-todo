5894025d7fb61d8453bee317680608d2dd21c4e3
yashicaj94 <yashicaj94@gmail.com>
2020-05-02 15:25:45 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
 import { TodoItemComponent } from './todo-item/todo-item.component';
 import { ListManagerComponent } from './list-manager/list-manager.component';
-import { TodoListService } from './todo-item/todo-list.service';
+import { TodoListService } from './services/todo-list.service';
 import { StorageService } from './services/storage.service';
 
 @NgModule({
